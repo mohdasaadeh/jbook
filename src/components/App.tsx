513f217aa2bc18efae6b7eb0dd8d9aa18516733d
mohdasaadeh <mass.1993@hotmail.com>
2022-07-23 21:56:20 +0300

    Adding the iframe with its functionality

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,9 +6,9 @@ import { onLoadPlugin } from "../plugins/onload-plugin";
 
 const App: React.FC = () => {
   const [input, setInput] = useState("");
-  const [code, setCode] = useState("");
 
   const esbuildRef = useRef<any>();
+  const iframeRef = useRef<any>();
 
   useEffect(() => {
     const startService = async () => {
@@ -22,6 +22,10 @@ const App: React.FC = () => {
   }, []);
 
   const onClick = async () => {
+    // This is when a user enters a code that deletes the root element inside the iframe,
+    // so the iframe gets reloaded with new root on every click.
+    iframeRef.current.srcdoc = html;
+
     const result = await esbuild.build({
       entryPoints: ["index.js"],
       bundle: true,
@@ -29,8 +33,28 @@ const App: React.FC = () => {
       plugins: [unpkgPathPlugin(), onLoadPlugin(input)],
     });
 
-    setCode(result.outputFiles[0].text);
+    iframeRef.current.contentWindow.postMessage(
+      result.outputFiles[0].text,
+      "*"
+    );
   };
+
+  const html = `
+    <html>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', async (event) => {
+            try {
+              eval(event.data);
+            } catch(error) {
+              document.getElementById('root').innerHTML = '<div style="color: red;">' + error + '</div>';
+            }
+          }, false);
+        </script>
+      </body>
+    </html>
+  `;
 
   return (
     <div>
@@ -38,7 +62,12 @@ const App: React.FC = () => {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
+      <iframe
+        ref={iframeRef}
+        title="preview"
+        sandbox="allow-scripts"
+        srcDoc={html}
+      />
     </div>
   );
 };
