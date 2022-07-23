932ab5db675c441a252b88fed3832232331a507a
mohdasaadeh <mass.1993@hotmail.com>
2022-07-23 09:02:30 +0300

    Bundling CSS files

--- src/plugins/onload-plugin.ts
+++ src/plugins/onload-plugin.ts
@@ -20,13 +20,47 @@ export const onLoadPlugin = (inputCode: string) => {
         }
       );
 
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        const storedPackage = await store.getItem<OnLoadResult>(args.path);
+
+        if (storedPackage) return storedPackage;
+      });
+
+      build.onLoad(
+        { filter: /.css$/ },
+        async (args: any): Promise<OnLoadResult> => {
+          const { data, request } = await axios.get(args.path);
+
+          const escapedData = data
+            .replace(/\n/g, "")
+            .replace(/"g/, '\\"')
+            .replace(/'g/, "\\'");
+
+          const contents = `
+            const style = document.createElement('style');
+
+            style.innerText = '${escapedData}';
+
+            document.head.appendChild(style);
+          `;
+
+          const responsePathname = new URL(".", request.responseURL).pathname;
+
+          const result: OnLoadResult = {
+            loader: "jsx",
+            contents,
+            resolveDir: responsePathname,
+          };
+
+          await store.setItem(args.path, result);
+
+          return result;
+        }
+      );
+
       build.onLoad(
         { filter: /.*/ },
         async (args: any): Promise<OnLoadResult> => {
-          const storedPackage = await store.getItem<OnLoadResult>(args.path);
-
-          if (storedPackage) return storedPackage;
-
           const { data, request } = await axios.get(args.path);
 
           const responsePathname = new URL(".", request.responseURL).pathname;
